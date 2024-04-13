14d2f2aafe05f1c8731e3c0713e7956782019052
James Dempsey <jd15497@my.bristol.ac.uk>
2024-04-13 21:30:11 +0100

    Fixed bugs

--- youTube/youTube.js
+++ youTube/youTube.js
@@ -1,3 +1,50 @@
-var images = document.getElementsByTagName("video");
+// var menuItems = document.getElementsByClassName("ytp-menuitem-content");
+// var menuOptions = document.getElementsByClassName("ytp-menuitem-label");
 
-images[0].playbackRate = 1.1;
+const observer = new MutationObserver((mutations) => {
+  // Step 2: Define the callback
+  mutations.forEach((mutation) => {
+    switch (mutation.type) {
+      case "attributes":
+        try {
+          // Handle attribute changes
+          const changedNode = mutation.target;
+          const changedAttribute = mutation.attributeName;
+          const newValue = changedNode.getAttribute(changedAttribute);
+          if (
+            mutation.target.children[2].children[1].innerHTML ==
+            "Playback speed"
+          ) {
+            var images = document.getElementsByTagName("video");
+            images[0].playbackRate;
+            mutation.target.children[2].children[1].parentElement.children[2].innerHTML = `${images[0].playbackRate} because we fast`;
+          }
+        } catch {}
+    }
+  });
+});
+
+const observerVideo = new MutationObserver((mutations) => {
+  // Step 2: Define the callback
+  mutations.forEach((mutation) => {
+    switch (mutation.type) {
+      case "attributes":
+        try {
+          var images = document.getElementsByTagName("video");
+          images[0].playbackRate = 1.1;
+        } catch {}
+    }
+  });
+});
+
+// Step 4: Configure and attach the observer
+const config = { childList: true, attributes: true, subtree: true }; // Observe direct children and descendants
+
+observer.observe(document.querySelector("#ytp-id-18 > div > div"), config); // Attach observer to the body element
+
+console.log(config);
+
+observerVideo.observe(
+  document.querySelector("#movie_player > div.html5-video-container"),
+  config
+); // Attach observer to the body element
