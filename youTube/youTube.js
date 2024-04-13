3e147e62b98c1ed0bc06d936166bd37ba4ee7378
James Dempsey <jd15497@my.bristol.ac.uk>
2024-04-13 15:05:57 +0100

    Tidy up

--- youTube/youTube.js
+++ youTube/youTube.js
@@ -1,22 +1,3 @@
-console.log("Dem-C extension loaded");
-
 var images = document.getElementsByTagName("video");
 
-console.log(images);
-console.log(images[0]);
-
 images[0].playbackRate = 1.1;
-
-// Step 1: Create a MutationObserver
-const observer = new MutationObserver((mutations) => {
-  // Step 2: Define the callback
-  mutations.forEach((mutation) => {
-    switch (mutation.type) {
-      case "attributes":
-        // Handle attribute changes
-        const changedNode = mutation.target;
-        console.log("mutation");
-        console.log(changedNode);
-    }
-  });
-});
